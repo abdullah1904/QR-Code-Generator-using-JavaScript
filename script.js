7ae8e30cc097b15b7bfa858402a04f45f72dae8c
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2024-03-16 15:29:10 +0500

    Styling

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const QRText = document.getElementById('QRText');
 const button = document.getElementById('generateButton');
 
 const generateQR = ()=>{
-    const QRApi = `https://chart.googleapis.com/chart?cht=qr&chf=bg,s,65432100&chs=${QRSize.value}&chl=${QRText.value}`;
-    QRImg.src = QRApi;
+    if(QRText.value !== ""){
+        const QRApi = `https://chart.googleapis.com/chart?cht=qr&chf=bg,s,65432100&chs=${QRSize.value}&chl=${QRText.value}`;
+        QRImg.src = QRApi;
+    }
+    else{
+        window.alert('Empty Text! Enter Text for QR Code')
+    }
 }
 
 button.addEventListener('click',generateQR)
